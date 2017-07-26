13969e170c50d7bb8207437bbc09baaaeac98067
Ridwan <m.ridwanfathin@gmail.com>
2017-07-26 21:07:21 +0700

    sql added

---
+++ src/view/StudentViewHandler.java
@@ -0,0 +1,50 @@
+package view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import sqlautograde.MainModel;
+import sqlautograde.Teacher;
+
+public class StudentViewHandler implements ActionListener {
+
+    private MainModel model;
+    private Main view;
+    private Statement statement = null;
+
+    public StudentViewHandler(Teacher p) {
+        model = new MainModel(p);
+        view = new Main();
+        view.setVisible(true);
+        view.addActionListener(this);
+        view.setLocationRelativeTo(null);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object source = ae.getSource();
+        try {
+             if (source.equals(view.getBtnTambah())) {
+                if (view.getKode().equals("") && view.getSoal().equals("") && view.getQuery().equals("")) {
+                    JOptionPane.showMessageDialog(view, "Nama tidak boleh kosong", "Input Kosong", 0);
+                } else {
+                    model.tambahSoal(Integer.parseInt(view.getKode()), view.getSoal(), view.getQuery());
+                }
+            }else if (source.equals(view.getBtnQuery()))  {
+                try {
+                    String query = view.getQuery();
+                    System.out.println(query);
+                    model.outputQuery(query);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("terjadi kesalahan saat mengeksekusi query");
+                }
+             } 
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(view, e.getMessage());
+        }
+    }
+}
